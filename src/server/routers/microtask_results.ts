ccc63ea6688b09f99609876ceaddb92fc80a6e6d
unvalley <kirohi.code@gmail.com>
2022-11-20 16:16:22 +0900

    feat: update microtask status when compelted

--- src/server/routers/microtask_results.ts
+++ src/server/routers/microtask_results.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { router } from "../trpc";
 import { publicProcedure } from "../trpc";
 import { prisma } from "../../lib/prismaClient";
+import { MicrotaskStatus } from ".prisma/client";
 
 export const microtaskResultsRouter = router({
   completeMicrotask: publicProcedure
@@ -12,6 +13,15 @@ export const microtaskResultsRouter = router({
       })
     )
     .mutation(async ({ input }) => {
+      // Update Status
+      await prisma.microtask.update({
+        where: { id: input.microtaskId },
+        data: {
+          status: MicrotaskStatus.DONE,
+        },
+      });
+
+      // Create Result
       await prisma.microtaskResult.create({
         data: {
           microtaskId: input.microtaskId,
