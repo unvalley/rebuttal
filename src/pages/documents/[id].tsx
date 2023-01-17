6846f1c243cb28c522d0064659d8159e99c15579
unvalley <kirohi.code@gmail.com>
2023-01-18 05:39:58 +0900

    update

--- src/pages/documents/[id].tsx
+++ src/pages/documents/[id].tsx
@@ -4,7 +4,6 @@ import { Layout } from "../../elements/Layout";
 import { FeedbackDocument } from "../../elements/Documents/FeedbackDocument";
 import { useRouter } from "next/router";
 import { ScreenLoading } from "../../elements/Parts/Loading";
-import { MICROTASKS } from "../../../constants/microtasks";
 import { MyError } from "../../elements/Parts/Error";
 
 const textColorByIsFact = (isFact: boolean) => {
@@ -77,10 +76,10 @@ const Documents = () => {
 
   const { data: aggregatedResults } = aggregatedResultsQuery;
   const { data: document } = documentQuery;
-  const { data: microtasks } = microtasksQuery;
-  const resultCount = microtasks
-    .flatMap((m) => m.microtaskResults.length)
-    .reduce((sum, e) => sum + e, 0);
+  // const { data: microtasks } = microtasksQuery;
+  // const resultCount = microtasks
+  //   .flatMap((m) => m.microtaskResults.length)
+  //   .reduce((sum, e) => sum + e, 0);
 
   // console.log(aggregatedResults.map((e) => e.opinonValidnessResults));
 
@@ -103,6 +102,28 @@ const Documents = () => {
   return (
     <div className="container mx-auto p-4">
       <h2 className="text-xl font-bold">論述改善フィードバックページ</h2>
+      <div className="collapse mt-4">
+        <input type="checkbox" className="peer" />
+        <div className="collapse-title bg-base-200">
+          <span className="font-bold">説明事項</span>
+        </div>
+        <div className="collapse-content bg-base-300">
+          <p>クラウドワーカに，文章のフィードバックを実施してもらいました．</p>
+          <ul>
+            <li>1. 各文を「意見」「事実」「どちらともいえない」に切り分ける</li>
+            <li>
+              2.
+              「事実」と区別された文に対して，根拠となる情報源が周囲に書かれているか確認
+            </li>
+            <li>
+              3.
+              「意見」と区別された文に対して，妥当な根拠が書かれているか確認．書かれていなければ，どう改善すればよいかを記述．
+            </li>
+          </ul>
+          <p>フィードバックを閲覧してみてください．</p>
+        </div>
+      </div>
+
       <div className="grid grid-cols-8 gap-2 mt-4">
         <div className="col-span-4">
           <div className="bg-base-200 p-2">
@@ -120,13 +141,11 @@ const Documents = () => {
 
         <div className="col-span-4">
           <div className="bg-base-200 p-2">
-            <div className="font-bold">
-              フィードバック ({resultCount}件が完了済み)
-            </div>
+            <div className="font-bold">フィードバック</div>
           </div>
           <div>
             {/* // fix style */}
-            <div className="overflow-scroll" style={{ height: "740px" }}>
+            <div className="overflow-y-scroll" style={{ height: "880px" }}>
               {aggregatedResults.map((result, idx) => {
                 // リファクタ & 数によって見せるか否か
                 const opinionValidnessCount = getResultCount(
@@ -140,8 +159,37 @@ const Documents = () => {
                 if (result.isFact && resourceCheckCount.resultCount === 0) {
                   return null;
                 }
-
                 if (!result.isFact && opinionValidnessCount.resultCount === 0) {
+                  return null;
+                }
+
+                const resourceCheckNegativeFeedbackCount =
+                  resourceCheckCount.falseCount +
+                  resourceCheckCount.lowReliabilityCount;
+                const resourceCheckPositiveFeedbackCount =
+                  resourceCheckCount.resultCount -
+                  resourceCheckNegativeFeedbackCount;
+
+                const shouldFixResourceCheck =
+                  resourceCheckNegativeFeedbackCount >=
+                  resourceCheckPositiveFeedbackCount;
+
+                if (result.isFact && !shouldFixResourceCheck) {
+                  return null;
+                }
+
+                const opinionValidnessNegativeFeedbackCount =
+                  opinionValidnessCount.falseCount +
+                  opinionValidnessCount.lowReliabilityCount;
+                const opinionValidnessPositiveFeedbackCount =
+                  opinionValidnessCount.resultCount -
+                  opinionValidnessNegativeFeedbackCount;
+
+                const shouldFixOpinionValidness =
+                  opinionValidnessNegativeFeedbackCount >=
+                  opinionValidnessPositiveFeedbackCount;
+
+                if (!result.isFact && !shouldFixOpinionValidness) {
                   return null;
                 }
 
@@ -181,17 +229,19 @@ const Documents = () => {
                             </span>
                             <div className="flex items-center justify-end">
                               <span className="text-md font-normal">
-                                根拠の欠落
+                                事実の根拠となる情報源の不足
                               </span>
                               <span>・</span>
                               <span className="font-light text-md text-slate-500">
-                                信頼できる根拠が必要
+                                文献情報を探しましょう
                               </span>
                             </div>
                           </div>
                           <div className="collapse-content">
-                            <div className="">
-                              {result.sentence?.body} (id: {result.sentenceId})
+                            <div>
+                              <span className="bg-indigo-100">
+                                {result.sentence?.body}
+                              </span>
                             </div>
                             <div className="mt-2">
                               <ResourceCheckFeedback
@@ -229,7 +279,9 @@ const Documents = () => {
                           </div>
                           <div className="collapse-content">
                             <div className="">
-                              {result.sentence?.body} (id: {result.sentenceId})
+                              <span className="bg-orange-100 italic font-semibold">
+                                {result.sentence?.body}
+                              </span>
                             </div>
                             <div className="mt-2">
                               <OpinionValidnessFeedback
@@ -256,30 +308,47 @@ const OpinionValidnessFeedback: React.FC<{
   count: {
     resultCount: number;
     falseCount: number;
+    lowReliabilityCount: number;
   };
   results: Result[];
 }> = ({ count, results }) => {
   return (
     <>
       <div>
-        {count.resultCount}
-        人が，この文章において，「
-        <span className="font-semibold">
-          {MICROTASKS.CHECK_OPINION_VALIDNESS}
+        上記の文に対して，
+        <span className="font-semibold italic">{count.resultCount}</span>人中，
+        <span className="font-semibold italic">
+          {count.falseCount + count.lowReliabilityCount}
         </span>
-        」を行いました． そのうちの
-        {count.falseCount}人が「
-        <span className="font-semibold">
-          妥当な根拠となる情報が書かれていない
-        </span>
+        人が「
+        <span className="">妥当な根拠が無い</span>
         」と述べています． 具体的に，以下のようなフィードバックが得られました．
       </div>
+
       <div className="mt-4">
-        {results.map((o, idx) => (
-          <div key={idx}>
-            <span className="text-lg">👤 : {o.reason != null && o.reason}</span>
-          </div>
-        ))}
+        {results.map((o, idx) => {
+          if (o.value === "TRUE") {
+            return <></>;
+          }
+          const answered =
+            o.value === "FALSE"
+              ? "根拠が書かれていない"
+              : "根拠は書かれているが，妥当とはいえない";
+
+          return (
+            <div className="mt-3" key={idx}>
+              <div className="">
+                👤 （<span className="italic">{answered}</span> と回答）:
+              </div>
+              <div className="font-semibold text-slate-800">
+                根拠を持たせられるように，改善するには？：
+              </div>
+              <span className="text-base text-slate-600">
+                {o.reason != null && o.reason}
+              </span>
+            </div>
+          );
+        })}
       </div>
     </>
   );
@@ -289,29 +358,21 @@ const ResourceCheckFeedback: React.FC<{
   count: {
     resultCount: number;
     falseCount: number;
+    lowReliabilityCount: number;
   };
   results: Result[];
-}> = ({ count, results }) => {
-  const lowReliabilityCount = results.filter(
-    (r) => r.value === "LOW_RELIABILITY"
-  ).length;
-
+}> = ({ count }) => {
   return (
     <>
       <div>
-        {count.resultCount}
-        人が，この文章において，「
-        <span className="font-semibold">{MICROTASKS.CHECK_FACT_RESOURCE}</span>
-        」を行いました． そのうちの
-        {count.falseCount}人が「
-        <span className="font-semibold">
-          妥当な根拠となる情報が書かれていない
+        上記の文に対して，
+        <span className="font-semibold italic">{count.resultCount}</span>人中，
+        <span className="font-semibold italic">
+          {count.falseCount + count.lowReliabilityCount}
         </span>
-        」と述べています．
-        <span>
-          また，{lowReliabilityCount}
-          人が，「根拠は書かれているが妥当ではない」と述べています．
-        </span>
+        人が「
+        <span className="">妥当な根拠となる情報が書かれていない</span>
+        」と述べています．根拠となるデータや参考文献を探して，より説得力を向上させましょう．
       </div>
     </>
   );
@@ -324,6 +385,8 @@ type Result = {
 const getResultCount = (results: Result[]) => {
   return {
     falseCount: results.filter((e) => e.value === "FALSE").length,
+    lowReliabilityCount: results.filter((e) => e.value === "LOW_RELIABILITY")
+      .length,
     resultCount: results.length,
   };
 };
